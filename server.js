af4b5dbe26ea7311356a77d1dadd1fac74e3a9b8
Aaron Allen <aaronseth.allen@gmail.com>
2024-07-03 06:37:27 -0400

    Started putting together server and basic, non-modular routes

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const path = require('path');
+const db = require('./db/db.json');
+const fs = require('fs');
+const PORT = 3001;
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+app. use(express.static('public'));
+
+app.get('/', (req, res)=>{
+    res.sendFile(path.join(__dirname, '/public/index.html'));
+});
+
+app.get(`/notes`, (req,res)=>{
+    res.sendFile(path.join(__dirname, '/public/notes.html'));
+});
+
+app.get(`/api/notes`, (req, res)=>{
+    console.info(`${req.method} request received to get notes.`);
+
+    return res.json(db);
+});
+app.post(`/api/notes`);
+app.delete(`/api/notes/:id`);
+
+app.listen(PORT, ()=>{
+    console.log(`Listening at http://localhost:${PORT}`)
+})
