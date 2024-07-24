68a6390276bba48a47217c79af31785c39bc48a8
Aaron Allen <aaronseth.allen@gmail.com>
2024-07-24 00:40:33 -0400

    updated port

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 const util = require('util');
 
 const db = require('./db/db.json');
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 
 const app = express();
