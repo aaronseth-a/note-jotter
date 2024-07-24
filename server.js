e7c247efde48ce2e032983cb7a3634a8046834d0
Aaron Allen <aaronseth.allen@gmail.com>
2024-07-23 23:39:41 -0400

    Final push. Completed

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
+const util = require('util');
 
 const db = require('./db/db.json');
 const PORT = 3001;
@@ -23,10 +24,10 @@ app.get(`/notes`, (req,res)=>{
 
 app.get(`/api/notes`, (req, res)=>{
     console.info(`${req.method} request received to get notes.`);
+    // try{res.json(db);}catch(err){res.status(500).json({error: "No notes to retrieve"})}
+    const readFromFile = util.promisify(fs.readFile);
+    readFromFile('./db/db.json').then((data)=>res.json(JSON.parse(data)));
 
-
-    res.json(db);
-    
 });
 
 app.post(`/api/notes`, (req,res)=>{
@@ -40,30 +41,59 @@ app.post(`/api/notes`, (req,res)=>{
             title,
             text
         }
-        db.push(newNote);
-        fs.writeFile('./db/db.json',JSON.stringify(db),(err) =>
-            err ? console.error(err) : console.log('File creation was successful!')
-        );
-        return res.status(200).send('Note saved successfully');
+
+        // db.push(newNote);
+        // fs.writeFile('./db/db.json',JSON.stringify(db),(err) =>
+        //     err ? console.error(err) : console.log('File creation was successful!')
+        // );
+
+        fs.readFile('./db/db.json', 'utf8', (err,data)=>{
+            if(err){
+                console.error(err);
+            }else{
+                const parsed = JSON.parse(data);
+                parsed.push(newNote);
+                fs.writeFile('./db/db.json',JSON.stringify(parsed),(err) =>
+                    err ? console.error(err) : console.log('File creation was successful!')
+                );
+            }
+            return res.status(200).send('Note saved successfully');
+        });
     }
+        
 });
 
 app.delete(`/api/notes/:id`, (req,res)=>{
-    debugger;
+    
     console.info(`${req.method} request received to delete a note with id#${req.params.id}.`);
     const targetId = parseInt(req.params.id);
-  
-        const updatedDB = db.filter(note=>note.id !== targetId);
-        fs.writeFile('./db/db.json',JSON.stringify(updatedDB),(err) =>
-            err ? console.error(err) : console.log('Note deletion was successful!')
-        );
+    
+    fs.readFile('./db/db.json', 'utf8', (err,data)=>{
+        if(err){
+            console.error(err);
+        }else{
+            const parsed = JSON.parse(data);
+            
+            const updatedDB = parsed.filter(note=>note.id !== targetId);
+            
+            fs.writeFile('./db/db.json',JSON.stringify(updatedDB),(err) =>
+                err ? console.error(err) : console.log('File creation was successful!')
+            );
+        }
+        return res.status(200).send('Note deleted successfully');
+    });
+    
+    // const updatedDB = db.filter(note=>note.id !== targetId);
+    // fs.writeFile('./db/db.json',JSON.stringify(updatedDB),(err) =>
+    //     err ? console.error(err) : console.log('Note deletion was successful!')
+    // );
 
-        const response ={
-            status: 'success',
-            body: updatedDB
-        };
-        
-        res.json(response);
+    // const response ={
+    //     status: 'success',
+    //     body: updatedDB
+    // };
+    
+    //res.json(response);
 });
 
 app.listen(PORT, ()=>{
